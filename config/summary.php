6543827728eb1cc63ebd57f02e43b3fae76e0ca7
onep2p <324834500@qq.com>
2018-01-24 17:49:39 +0800

    update

--- config/summary.php
+++ config/summary.php
@@ -10,8 +10,8 @@ return [
     'icon'  => 'ios-paper-outline',//模块栏目图标
     'path' => url(app\news\controller\admin\News::class.'@store'),//模块入口地址(路由)
     
-    'open' => 'news',//默认打开的侧边一级栏目
-    'active' => 'news-list',//默认页面指定
+    'open' => ['news'],//默认打开的侧边一级栏目  必须同控制器名
+    'active' => 'store',//默认页面指定 必须同操作名
     
     'title' => '新闻',//面包屑一级名称
     'subTitle' => '新闻管理',//面包屑二级名称
@@ -22,14 +22,14 @@ return [
             'alias' => 'news',
             'icon' => 'ios-list-outline',
             'submenu' => [
-                ['name' => '新闻列表', 'alias' => 'news-list', 'icon'  => '', 'path'  => ''],
+                ['name' => '新闻列表', 'alias' => 'store', 'icon'  => '', 'path'  => ''],
                 ['name' => '草稿箱', 'alias' => 'user-role', 'icon'  => '', 'path'  => ''],
                 ['name' => '废纸篓', 'alias' => 'access-list', 'icon'  => '', 'path'  => ''],
             ]
         ],
         [
             'name' => '分类管理',
-            'alias' => '',
+            'alias' => 'cate',
             'icon' => 'ios-color-filter',
             'submenu' => [
                 ['name' => '新闻分类', 'alias' => '', 'icon'  => '', 'path'  => ''],
